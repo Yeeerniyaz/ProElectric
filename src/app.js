c2afb2e6d674a8c2787b910e965e77e544e4af2d
Yerniyaz <t.yerniyaz@gmail.com>
2026-02-18 12:39:20 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ import { OrderService } from "./services/OrderService.js";
 
 // --- INITIALIZATION ---
 const app = express();
+app.set("trust proxy", 1);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
